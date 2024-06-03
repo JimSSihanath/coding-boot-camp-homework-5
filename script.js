0c1d6e99ca95609b788b3dab81f7478979e95e9b
Jim S. Sihanath <j.ssihanath@gmail.com>
2024-06-02 18:09:05 -0600

    Created taskCard Variable.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const taskCard = document.createElement('div');
 }
 
 // Todo: create a function to render the task list and make cards draggable
